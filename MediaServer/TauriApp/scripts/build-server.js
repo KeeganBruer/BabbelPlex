c9ccff8146db22d965c5fab16b34cd61da9b3744
Keegan Bruer <keegan.bruer@gmail.com>
2025-02-24 20:48:13 -0500

    node server

--- MediaServer/TauriApp/scripts/build-server.js
+++ MediaServer/TauriApp/scripts/build-server.js
@@ -1,24 +1,6 @@
 import { execSync } from 'child_process';
 import fs from 'fs';
 
-
-
-console.log("SERVER DIR", process.cwd() + "/server")
-
-console.log("npm install")
-const Ha = execSync('npm install', { cwd: process.cwd() + "/server" });
-
-console.log("npx tsc")
-try {
-
-    const Ha1 = execSync('npx tsc', { cwd: process.cwd() + "/server" });
-    console.log("Ha1", Ha1.toString());
-} catch (e) {
-    console.log("Ha1 err",e)
-}
-
-const Ha2 = execSync('npx pkg .', { cwd: process.cwd() + "/server" });
-
 const ext = process.platform === 'win32' ? '.exe' : '';
 
 const rustInfo = execSync('rustc -vV');
