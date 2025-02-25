a2cbcb4511e81dcbd87e91787c92cd3e3a113e2f
Keegan Bruer <keegan.bruer@gmail.com>
2025-02-24 20:33:01 -0500

    node server

--- MediaServer/TauriApp/scripts/build-server.js
+++ MediaServer/TauriApp/scripts/build-server.js
@@ -3,9 +3,10 @@ import fs from 'fs';
 
 
 
-const Ha = execSync('npm install', { cwd:process.cwd()+"/server", stdio:"pipe"});
-const Ha1 = execSync('npx tsc', { cwd:process.cwd()+"/server", stdio:"pipe"});
-const Ha2 = execSync('npx pkg .', { cwd:process.cwd()+"/server", stdio:"pipe"});
+const Ha = execSync('npm install', { cwd:process.cwd()+"/server"});
+const Ha1 = execSync('npx tsc', { cwd: process.cwd() + "/server" });
+console.log("Ha1", Ha1.toString());
+const Ha2 = execSync('npx pkg .', { cwd:process.cwd()+"/server"});
 
 const ext = process.platform === 'win32' ? '.exe' : '';
 
