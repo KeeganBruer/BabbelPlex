2f4228183a73df4e884cd37c11035cd7c08e44da
Keegan Bruer <keegan.bruer@gmail.com>
2025-02-24 21:00:36 -0500

    node server

--- MediaServer/TauriApp/scripts/build-server.js
+++ MediaServer/TauriApp/scripts/build-server.js
@@ -1,7 +1,11 @@
 import { execSync } from 'child_process';
 import fs from 'fs';
 
-const ext = process.platform === 'win32' ? '.exe' : '';
+const ext = process.platform === 'win32'
+    ? '-win.exe'
+    : process.platform === 'linux'
+        ? '-linux'
+        :'-macos';
 
 const rustInfo = execSync('rustc -vV');
 const targetTriple = /host: (\S+)/g.exec(rustInfo)[1];
